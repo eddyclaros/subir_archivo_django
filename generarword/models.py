119d1c963300dc986c2568576635dafe910c8390
Eddy Claros <eclaros@segurosmsc.com>
2022-06-27 16:03:22 -0400

    modelo creado

--- generarword/models.py
+++ generarword/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Clausulas(models.Model):
+    id=models.AutoField(primary_key=True)
+    title=models.CharField(max_length=100, null=True, verbose_name="Título")
+    subtitle=models.CharField(max_length=100,null=True, verbose_name="Subtitulo")
+    numpoliza=models.CharField(max_length=50,null=True, verbose_name="Numero poliza")
+    nameaseg=models.CharField(max_length=50,null=True, verbose_name="Asegurado" )
+    textpoliza=models.TextField(verbose_name="Parrafos")
+
