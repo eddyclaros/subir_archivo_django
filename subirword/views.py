25200f6760a674d410915c8c1a4a9be66e13c900
Eddy Claros <eclaros@segurosmsc.com>
2022-06-28 15:49:01 -0400

    corregido carpeta clausulas

--- subirword/views.py
+++ subirword/views.py
@@ -7,7 +7,28 @@ from subirword.models import ArchivoClausula
 # Create your views here.
 class SubirWord():
     def subirword(request):
-        if request.method=='POST' and request.FILES['myfile']:
+        if request.method == "POST":
+            # Fetching the form data
+            fileTitle = request.POST["filename"]
+            uploadedFile = request.FILES["myfile"]
+
+            # Saving the information in the database
+            document = ArchivoClausula(
+                title = fileTitle,
+                file = uploadedFile
+            )
+            document.save()
+
+        documents = ArchivoClausula.objects.all()
+
+        return render(request, 'paginas/subir.html',{'clausulas':documents})
+
+
+
+
+
+
+        """ if request.method=='POST' and request.FILES['myfile']:
             nombre=request.POST['filename']
             print("entra")
             myfile=request.FILES['myfile']
@@ -22,4 +43,4 @@ class SubirWord():
             clausulas=ArchivoClausula.objects.all()
             return render(request, 'paginas/subir.html',{'clausulas':clausulas})
         clausulas=ArchivoClausula.objects.all()
-        return render(request,"paginas/subir.html",{'clausulas':clausulas})
+        return render(request,"paginas/subir.html",{'clausulas':clausulas}) """
