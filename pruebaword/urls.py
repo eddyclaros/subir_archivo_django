4f3b5f9a6121b3cb53c85bf44876635baacb6139
Eddy Claros <eclaros@segurosmsc.com>
2022-06-28 15:55:44 -0400

    archivos subidos

--- pruebaword/urls.py
+++ pruebaword/urls.py
@@ -28,6 +28,6 @@ urlpatterns = [
     path('subir/', include('subirword.urls')),
 ]
 
-#subir archivos
+#agregar para subir archivos
 if settings.DEBUG:
-        urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
